dcdc3773c68be5966901f617442512f5a81ada25
sashasec <50148159+sashasec@users.noreply.github.com>
2021-06-26 21:34:19 -0600

    Update test.py
    fixed indentation.

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-        import time
-        from blink1.blink1 import Blink1
+import time
+from blink1.blink1 import Blink1
     
-        blink1 = Blink1()
-        blink1.fade_to_rgb(1000, 0, 128, 0, 0) #green
-        time.sleep(3)
-        blink1.fade_to_rgb(1000, 255, 255, 102, 0) #yellow
-        time.sleep(3)
-        blink1.fade_to_rgb(1000, 255, 0, 0, 0) #red
+blink1 = Blink1()
+blink1.fade_to_rgb(1000, 0, 128, 0, 0) #green
+time.sleep(3)
+blink1.fade_to_rgb(1000, 255, 255, 102, 0) #yellow
+time.sleep(3)
+blink1.fade_to_rgb(1000, 255, 0, 0, 0) #red
