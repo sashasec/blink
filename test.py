e3352f2dc6331db1258d3c7f3618c2b5355a368b
sashasec <50148159+sashasec@users.noreply.github.com>
2021-06-26 21:52:13 -0600

    Update test.py
    modified colors to be more CISA/FIRST compliant.

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import time
 from blink1.blink1 import Blink1
     
 blink1 = Blink1()
-blink1.fade_to_rgb(1000, 0, 128, 0, 0) #green
+blink1.fade_to_rgb(1000, 51, 255, 0, 0) #green
 time.sleep(3)
-blink1.fade_to_rgb(1000, 255, 255, 102, 0) #yellow
+blink1.fade_to_rgb(1000, 255, 255, 255, 0) #white 
+time.sleep(3)
+blink1.fade_to_rgb(1000, 255, 192, 0, 0) #amber
 time.sleep(3)
 blink1.fade_to_rgb(1000, 255, 0, 0, 0) #red
